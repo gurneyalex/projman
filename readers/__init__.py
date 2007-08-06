6a046fd91ae0cb8bab8f7845b8d4336e6f621395
ludal <ludal@logilab.fr>
2007-08-06 18:17:01 +0200

    replace projman_reader with elementtree-based parser pyxml is going soon ...

--- readers/__init__.py
+++ readers/__init__.py
@@ -15,6 +15,7 @@
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 """ external access point to readers base classes """
 
-from projman.readers.projman_reader import ProjectXMLReader, TaskXMLReader, \
-                                           ResourcesXMLReader, ScheduleXMLReader
+## from projman.readers.projman_reader import ProjectXMLReader, TaskXMLReader, \
+##                                            ResourcesXMLReader, ScheduleXMLReader
 from projman.readers.planner_reader import PlannerXMLReader
+from projman.readers.projman_reader import ProjectXMLReader
