d173a38fa4aa430021075239aff5ce028bbd1a23
Charlotte Le Calvez <charlotte.lecalvez@logilab.fr>
2009-02-25 14:03:47 +0100

    prepare version 0.13.6

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -18,7 +18,7 @@ http://www.logilab.fr/ -- mailto:contact@logilab.fr
 
 modname = 'projman'
 
-numversion = (0, 13, 5)
+numversion = (0, 13, 6)
 version = '.'.join([str(num) for num in numversion])
 
 
