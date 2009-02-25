6f8a0c50ade407a19066392faabc4ea0c99998a5
Charlotte Le Calvez <charlotte.lecalvez@logilab.fr>
2009-02-25 14:19:40 +0100

    update setup.py

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-"""Copyright (c) 2000-2008 LOGILAB S.A. (Paris, FRANCE).
+"""Copyright (c) 2000-2009 LOGILAB S.A. (Paris, FRANCE).
 http://www.logilab.fr/ -- mailto:contact@logilab.fr  
 """
 
@@ -23,7 +23,7 @@ version = '.'.join([str(num) for num in numversion])
 
 
 license = 'GPL'
-copyright = '''Copyright © 2000-2008 LOGILAB S.A. (Paris, FRANCE).
+copyright = '''Copyright © 2000-2009 LOGILAB S.A. (Paris, FRANCE).
 http://www.logilab.fr/ -- mailto:contact@logilab.fr'''
 
 short_desc = "tool for project management"
