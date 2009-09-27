6499d7e0ee8d361294715dd9fbeceb7f581d07c0
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2009-09-27 16:04:44 +0200

    [package] add dependencies

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -74,6 +74,9 @@ debian_maintainer_email = 'alexandre.fayolle@logilab.fr'
 pyversions = ["2.5"]
 
 __depends__ = {'logilab-common': '>= 0.44.0',
+               'python-gtksourceview': '>= 2.2.0',
+               'python-docutils': None,
+               'PIL': None,
                }
 
 from os.path import join
