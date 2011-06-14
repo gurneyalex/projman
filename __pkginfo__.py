749082c1af35308e1c5f87a9b8b7a728f3218927
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2011-06-14 14:50:58 +0200

    [packaging] prepare 0.15.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -12,17 +12,17 @@
 # You should have received a copy of the GNU General Public License along with
 # this program; if not, write to the Free Software Foundation, Inc.,
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-"""Copyright (c) 2000-2010 LOGILAB S.A. (Paris, FRANCE).
+"""Copyright (c) 2000-2011 LOGILAB S.A. (Paris, FRANCE).
 http://www.logilab.fr/ -- mailto:contact@logilab.fr
 """
 
 modname = 'projman'
 
-numversion = (0, 14, 0)
+numversion = (0, 15, 0)
 version = '.'.join([str(num) for num in numversion])
 
 license = 'GPL'
-copyright = '''Copyright © 2000-2010 LOGILAB S.A. (Paris, FRANCE).
+copyright = '''Copyright © 2000-2011 LOGILAB S.A. (Paris, FRANCE).
 http://www.logilab.fr/ -- mailto:contact@logilab.fr'''
 
 short_desc = "project management tool"
